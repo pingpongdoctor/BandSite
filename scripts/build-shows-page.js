32497de2388157df88d37783bf7c7aeb232431e1
Simon Tran <thanhnhantran1501@gmail.com>
2022-10-28 06:50:07 -0700

    finish getting data and redering by using javascript

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,68 +1,48 @@
-//THE ARRAY INCLUDES DATA OF THE SHOWS SECTION
-// const showsData = [
-//   {
-//     Date: "Mon Sept 06 2021",
+//TIMESTAMP CONVERT FUNCTION
+const timeStampConverter = function (timeStamp) {
+  const timeInfor = new Date(timeStamp);
+  const timeDisplay = timeInfor.toDateString();
+  return timeDisplay;
+};
 
-//     Venue: "Ronald Lane",
+//DECLARE A FUNCTION USED TO DECLARE ELEMENTS WITH A CLASS
+const createElementWithClass = function (element, className) {
+  const newShowsElement = document.createElement(element);
+  newShowsElement.classList.add(className);
+  return newShowsElement;
+};
 
-//     Location: "San Francisco, CA",
-//   },
-
-//   {
-//     Date: "Tue Sept 21 2021",
-
-//     Venue: "Pier 3 East ",
-
-//     Location: "San Francisco, CA",
-//   },
-
-//   {
-//     Date: "Fri Oct 15 2021",
-
-//     Venue: "View Lounge",
-
-//     Location: "San Francisco, CA",
-//   },
-
-//   {
-//     Date: "Sat Nov 06 2021",
-
-//     Venue: "Hyatt Agency",
-
-//     Location: "San Francisco, CA",
-//   },
-
-//   {
-//     Date: "Fri Nov 26 2021",
-
-//     Venue: "Moscow Center ",
-
-//     Location: "San Francisco, CA",
-//   },
-
-//   {
-//     Date: "Wed Dec 15 2021",
-
-//     Venue: "Press Club ",
-
-//     Location: "San Francisco, CA",
-//   },
-// ];
+const createElementWithClassAndInnertext = function (
+  element,
+  className,
+  innerText
+) {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(className);
+  createdElement.innerText = innerText;
+  return createdElement;
+};
 
 //BUILD THE ELEMENTS FOR THE SHOWS SECTION, THAT ONLY APPEAR IN THE TABLET BREAKPOINT AND DESKTOP BREAKPOINT
 const showsTabletDisplayWrap = document.querySelector(".shows__display-wrap");
 //Create tablet date
-const showsTabletDate = document.createElement("p");
-showsTabletDate.classList.add("shows__sub-header-display");
-showsTabletDate.innerText = "Date";
+const showsTabletDate = createElementWithClassAndInnertext(
+  "p",
+  "shows__sub-header-display",
+  "Date"
+);
 //Create tablet venue
-const showsTabletVenue = document.createElement("p");
-showsTabletVenue.classList.add("shows__sub-header-display");
-showsTabletVenue.innerText = "Venue";
+const showsTabletVenue = createElementWithClassAndInnertext(
+  "p",
+  "shows__sub-header-display",
+  "Venue"
+);
 //Create tablet location
-const showsTabletLocation = document.createElement("p");
-showsTabletLocation.classList.add("shows__sub-header-display");
-showsTabletLocation.innerText = "Location";
+const showsTabletLocation = createElementWithClassAndInnertext(
+  "p",
+  "shows__sub-header-display",
+  "Location"
+);
 //Create tablet button that is set display:visibility
 const visibilityDisplayButton = document.createElement("button");
 visibilityDisplayButton.classList.add("btn");
@@ -73,133 +53,131 @@ showsTabletDisplayWrap.appendChild(showsTabletVenue); //Append Tablet venue to d
 showsTabletDisplayWrap.appendChild(showsTabletLocation); //Append tablet Location to div
 showsTabletDisplayWrap.appendChild(visibilityDisplayButton); //Append tablet Button to div
 
-//DECLARE A FUNCTION USED TO DECLARE ELEMENTS WITH A CLASS
-const createElementWithClass = function (element, className) {
-  const newShowsElement = document.createElement(element);
-  newShowsElement.classList.add(className);
-  return newShowsElement;
+//HIGHLIGHT THE ROWS
+const highLight = function () {
+  const showItems = document.querySelectorAll(".shows__item");
+  for (let i = 0; i < showItems.length; i++) {
+    showItems[i].addEventListener("click", (event) => {
+      if (showItems[i].classList.contains("shows__row-active")) {
+        showItems[i].classList.remove("shows__row-active");
+      } else {
+        for (let a = 0; a < showItems.length; a++) {
+          showItems[a].classList.remove("shows__row-active");
+        }
+        showItems[i].classList.add("shows__row-active");
+      }
+    });
+  }
 };
-
-//TIMESTAMP CONVERT FUNCTION
-const timeStampConverter = function (timeStamp) {
-  const timeInfor = new Date(timeStamp);
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const currentDate = timeInfor.getDay();
-  const currentMonth = months[timeInfor.getMonth()];
-  const currentYear = timeInfor.getFullYear();
-  const currentHour = timeInfor.getHours();
-  const currentMinnute = timeInfor.getMinutes();
-  const timeDisplay = `0${currentDate}/${currentMonth}/${currentYear}`;
-  return timeDisplay;
+//SET MOUSEOVER AND MOUSE LEAVE
+const hoverFunction = function () {
+  const showItems = document.querySelectorAll(".shows__item");
+  showItems.forEach((showItem) => {
+    showItem.addEventListener("mouseover", () => {
+      showItem.classList.add("shows__row-hover");
+    });
+    showItem.addEventListener("mouseleave", () => {
+      showItem.classList.remove("shows__row-hover");
+    });
+  });
 };
-
-console.log(timeStampConverter(1630965369000));
 
 //GET DATA FROM SERVER
 const showsList = document.querySelector(".shows__list");
 const showsHeading = document.querySelector(".shows__heading");
 showsHeading.innerText = "Shows";
 
-const getData = axios
-  .get(
-    "https://project-1-api.herokuapp.com/showdates?api_key=b0519b09-2feb-4482-9102-0ec91faa6067"
-  )
-  .then((response) => {
-    console.log(response.data);
-    const showsData = response.data;
+const getDataAndDisPlay = function () {
+  const getData = axios
+    .get(
+      "https://project-1-api.herokuapp.com/showdates?api_key=b0519b09-2feb-4482-9102-0ec91faa6067"
+    )
+    .then((response) => {
+      const showsData = response.data;
 
-    showsList.innerHTML = "";
-    for (let i = 0; i < showsData.length; i++) {
-      //Create items (li tag)
-      const showsItem = createElementWithClass("li", "shows__item");
-      //Create a div for date and date detail
-      const divDateAndDateDetail = createElementWithClass("div", "shows__wrap");
-      //Create date
-      const showsDate = document.createElement("p");
-      showsDate.classList.add("shows__sub-header");
-      showsDate.innerText = "Date";
-      //Create date detail
-      const showsDateDetail = document.createElement("p");
-      showsDateDetail.classList.add("shows__date-detail");
-      showsDateDetail.innerText = timeStampConverter(showsData[i].date);
-      //Create a div for venue and venue detail
-      const divVenueAndVenueDetail = createElementWithClass(
-        "div",
-        "shows__wrap"
-      );
-      //Create venue
-      const showsVenue = document.createElement("p");
-      showsVenue.classList.add("shows__sub-header");
-      showsVenue.innerText = "Venue";
-      //Create venue detail
-      const showsVenueDetail = document.createElement("p");
-      showsVenueDetail.classList.add("shows__venue-detail");
-      showsVenueDetail.innerText = showsData[i].place;
-      //Create a div for location and location detail
-      const divLocationAndLocationDetail = createElementWithClass(
-        "div",
-        "shows__wrap"
-      );
-      //Create location
-      const showsLocation = document.createElement("p");
-      showsLocation.classList.add("shows__sub-header");
-      showsLocation.innerText = "Location";
-      //Create location detail
-      const showsLocationDetail = document.createElement("p");
-      showsLocationDetail.classList.add("shows__location-detail");
-      showsLocationDetail.innerText = showsData[i].location;
-      //Create a button
-      const showsButton = document.createElement("button");
-      showsButton.innerText = "buy tickets";
-      showsButton.classList.add("btn");
+      showsList.innerHTML = "";
+      showsData.forEach((showObject) => {
+        //Create items (li tag)
+        const showsItem = createElementWithClass("li", "shows__item");
+        //Create a div for date and date detail
+        const divDateAndDateDetail = createElementWithClass(
+          "div",
+          "shows__wrap"
+        );
+        //Create date
+        const showsDate = createElementWithClassAndInnertext(
+          "p",
+          "shows__sub-header",
+          "Date"
+        );
+        //Create date detail
 
-      //AppendChild
-      divDateAndDateDetail.appendChild(showsDate); //add date to div for date and date detail
-      divDateAndDateDetail.appendChild(showsDateDetail); //add date detail to div for date and date detail
-      divVenueAndVenueDetail.appendChild(showsVenue); //add venue to div for venue and venue detail
-      divVenueAndVenueDetail.appendChild(showsVenueDetail); //add venue detail to div for venue and venue detail
-      divLocationAndLocationDetail.appendChild(showsLocation); //add location to div for location and location detail
-      divLocationAndLocationDetail.appendChild(showsLocationDetail); //add location detail to  div for location and location detail
-      showsItem.appendChild(divDateAndDateDetail); //add div for date and date detail
-      showsItem.appendChild(divVenueAndVenueDetail); //add div for venue and venue detail to item
-      showsItem.appendChild(divLocationAndLocationDetail); //add div for location and location detail to item
-      showsItem.appendChild(showsButton); //add button to li
-      showsList.appendChild(showsItem); //add li to ul
-    }
-  });
-//HIGHLIGHT THE ROWS
-const showItems = document.querySelectorAll(".shows__item");
-for (let i = 0; i < showItems.length; i++) {
-  showItems[i].addEventListener("click", () => {
-    if (showItems[i].classList.contains("shows__row-active")) {
-      showItems[i].classList.remove("shows__row-active");
-    } else {
-      for (let a = 0; a < showItems.length; a++) {
-        showItems[a].classList.remove("shows__row-active");
-      }
-      showItems[i].classList.add("shows__row-active");
-    }
-  });
-}
-//SET MOUSEOVER AND MOUSE LEAVE
-for (let i = 0; i < showItems.length; i++) {
-  showItems[i].addEventListener("mouseover", () => {
-    showItems[i].classList.add("shows__row-hover");
-  });
-  showItems[i].addEventListener("mouseleave", () => {
-    showItems[i].classList.remove("shows__row-hover");
-  });
-}
+        const showsDateDetail = createElementWithClassAndInnertext(
+          "p",
+          "shows__date-detail",
+          timeStampConverter(showObject.date)
+        );
+        //Create a div for venue and venue detail
+        const divVenueAndVenueDetail = createElementWithClass(
+          "div",
+          "shows__wrap"
+        );
+        //Create venue
+        const showsVenue = createElementWithClassAndInnertext(
+          "p",
+          "shows__sub-header",
+          "Venue"
+        );
+        //Create venue detail
+        const showsVenueDetail = createElementWithClassAndInnertext(
+          "p",
+          "shows__venue-detail",
+          showObject.place
+        );
+        //Create a div for location and location detail
+        const divLocationAndLocationDetail = createElementWithClass(
+          "div",
+          "shows__wrap"
+        );
+        //Create location
+        const showsLocation = createElementWithClassAndInnertext(
+          "p",
+          "shows__sub-header",
+          "Location"
+        );
+        //Create location detail
+        const showsLocationDetail = createElementWithClassAndInnertext(
+          "p",
+          "shows__location-detail",
+          showObject.location
+        );
+        //Create a button
+
+        const showsButton = createElementWithClassAndInnertext(
+          "button",
+          "btn",
+          "buy tickets"
+        );
+
+        //AppendChild
+        divDateAndDateDetail.appendChild(showsDate); //add date to div for date and date detail
+        divDateAndDateDetail.appendChild(showsDateDetail); //add date detail to div for date and date detail
+        divVenueAndVenueDetail.appendChild(showsVenue); //add venue to div for venue and venue detail
+        divVenueAndVenueDetail.appendChild(showsVenueDetail); //add venue detail to div for venue and venue detail
+        divLocationAndLocationDetail.appendChild(showsLocation); //add location to div for location and location detail
+        divLocationAndLocationDetail.appendChild(showsLocationDetail); //add location detail to  div for location and location detail
+        showsItem.appendChild(divDateAndDateDetail); //add div for date and date detail
+        showsItem.appendChild(divVenueAndVenueDetail); //add div for venue and venue detail to item
+        showsItem.appendChild(divLocationAndLocationDetail); //add div for location and location detail to item
+        showsItem.appendChild(showsButton); //add button to li
+        showsList.appendChild(showsItem); //add li to ul
+      });
+      highLight();
+      hoverFunction();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+getDataAndDisPlay();
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,27 +1,9 @@
-//THE ARRAY INCLUDES THE DATA OF THE COMMENTS
-// const commentData = [
-//   {
-//     name: "Connor Walton",
-//     commentText:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//     date: "02/17/2021",
-//     image: "./assets/Images/Mohan-muruge.jpg",
-//   },
-//   {
-//     name: "Emilie Beach",
-//     commentText:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//     date: "01/09/2021",
-//     image: "./assets/Images/Mohan-muruge.jpg",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     commentText:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//     date: "12/20/2020",
-//     image: "./assets/Images/Mohan-muruge.jpg",
-//   },
-// ];
+//TIMESTAMP CONVERT FUNCTION
+const timeStampConverter = function (timeStamp) {
+  const timeInfor = new Date(timeStamp);
+  const timeDisplay = timeInfor.toLocaleDateString();
+  return timeDisplay;
+};
 
 //ADD COMMENTS
 const conversationList = document.querySelector(".conversation__list");
@@ -31,46 +13,29 @@ const createElementWithClass = function (element, classes) {
   createdElement.classList.add(classes);
   return createdElement;
 };
-const addComment = function () {};
-
-//TIMESTAMP CONVERT FUNCTION
-const timeStampConverter = function (timeStamp) {
-  const timeInfor = new Date(timeStamp);
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const currentDate = timeInfor.getDay();
-  const currentMonth = months[timeInfor.getMonth()];
-  const currentYear = timeInfor.getFullYear();
-  const currentHour = timeInfor.getHours();
-  const currentMinnute = timeInfor.getMinutes();
-  const timeDisplay = `0${currentDate}/${currentMonth}/${currentYear}`;
-  return timeDisplay;
+const createElementWithClassAndInnertext = function (
+  element,
+  className,
+  innerText
+) {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(className);
+  createdElement.innerText = innerText;
+  return createdElement;
 };
 
-//Get data from Web API
+//GET DATA FROM API WEB
+const apiLink =
+  "https://project-1-api.herokuapp.com/comments?api_key=b0519b09-2feb-4482-9102-0ec91faa6067";
 
-const getData = function () {
+const getAndDisplayData = function () {
   axios
-    .get(
-      "https://project-1-api.herokuapp.com/comments?api_key=b0519b09-2feb-4482-9102-0ec91faa6067"
-    )
+    .get(apiLink)
     .then((response) => {
       console.log(response.data);
       const commentData = response.data;
-      conversationList.innerHTML = "";
 
+      conversationList.innerHTML = "";
       for (let i = commentData.length - 1; i >= 0; i--) {
         //Create item (li tag)
         const commentItem = createElementWithClass("li", "conversation__item");
@@ -90,24 +55,81 @@ const getData = function () {
         commentImg.src = "./assets/Images/Mohan-muruge.jpg";
         commentImg.alt = "avatar";
         //Create p name
-        const commentName = document.createElement("p");
-        commentName.classList.add("conversation__name");
-        commentName.innerText = commentData[i].name;
+        const commentName = createElementWithClassAndInnertext(
+          "p",
+          "conversation__name",
+          commentData[i].name
+        );
         //Create p date
-        const commentDate = document.createElement("p");
-        commentDate.classList.add("conversation__date");
-        commentDate.innerText = timeStampConverter(commentData[i].timestamp);
+        const commentDate = createElementWithClassAndInnertext(
+          "p",
+          "conversation__date",
+          timeStampConverter(commentData[i].timestamp)
+        );
         //Create p comment detail
         const commentDetail = document.createElement("p");
         commentDetail.innerText = commentData[i].comment;
+        //Create like button
+        const buttonLike = createElementWithClassAndInnertext(
+          "button",
+          "btn-like",
+          `Like: ${commentData[i].likes}`
+        );
+
+        //Create delete button
+        const buttonDelete = createElementWithClassAndInnertext(
+          "button",
+          "btn-delete",
+          "X"
+        );
+        //Create div for like and delete buttons
+        const commentLikeDelete = createElementWithClass(
+          "div",
+          "conversation__btn-wrap"
+        );
+
         //AppendChild
         commentDivNameAndDate.appendChild(commentName); //add namw to div of name and date
         commentDivNameAndDate.appendChild(commentDate); //add date to div of name and date
+        commentLikeDelete.appendChild(buttonLike);
+        commentLikeDelete.appendChild(buttonDelete);
         commentDiv.appendChild(commentDivNameAndDate); //add div of name and date to div of p tags
         commentDiv.appendChild(commentDetail); //add text to div of p tags
+        commentDiv.appendChild(commentLikeDelete);
         commentItem.appendChild(commentImg); //add img to item
         commentItem.appendChild(commentDiv); //add div for p tags to Item
         conversationList.appendChild(commentItem); //add item (li) to ul
+
+        //UPDATE THE LIKE BUTTON
+        const id = commentData[i].id;
+        buttonLike.addEventListener("click", () => {
+          axios
+            .put(
+              `https://project-1-api.herokuapp.com/comments/${id}/like?api_key=b0519b09-2feb-4482-9102-0ec91faa6067`
+            )
+            .then((response) => {
+              console.log(response);
+              getAndDisplayData();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+
+        //SET FOR THE DELETE BUTTON
+        buttonDelete.addEventListener("click", () => {
+          axios
+            .delete(
+              `https://project-1-api.herokuapp.com/comments/${id}?api_key=b0519b09-2feb-4482-9102-0ec91faa6067`
+            )
+            .then((response) => {
+              console.log(response);
+              getAndDisplayData();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
       }
     })
     .catch((error) => {
@@ -115,28 +137,30 @@ const getData = function () {
     });
 };
 
-getData();
-
-const input = document.querySelectorAll(".conversation__input");
-const form = document.querySelector(".conversation__form");
+getAndDisplayData();
 
 //SUBMIT FORM
+const input = document.querySelectorAll(".conversation__input");
+const form = document.querySelector(".conversation__form");
+class Comment {
+  constructor(name, comment) {
+    this.name = name;
+    this.comment = comment;
+  }
+}
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   if (event.target.name.value !== "" && event.target.comment.value !== "") {
-    const newComment = {
-      name: event.target.name.value,
-      comment: event.target.comment.value,
-    };
-    //Post newComment to server
+    const newComment = new Comment(
+      event.target.name.value,
+      event.target.comment.value
+    );
+    console.log(newComment);
     axios
-      .post(
-        "https://project-1-api.herokuapp.com/comments?api_key=b0519b09-2feb-4482-9102-0ec91faa6067",
-        newComment
-      )
+      .post(apiLink, newComment)
       .then((response) => {
         console.log(response);
-        getData();
+        getAndDisplayData();
       })
       .catch((error) => {
         console.log(error);
@@ -156,16 +180,16 @@ form.addEventListener("submit", (event) => {
   }
 });
 
-//Make the input box active when focus and not active when blur
-for (let i = 0; i < input.length; i++) {
+//SET FOCUS AND BLUR EVENTS FOR INPUT BOXES
+input.forEach((inputBox) => {
   //use focus event
-  input[i].addEventListener("focus", (event) => {
-    input[i].classList.add("conversation__input--focus");
-    input[i].classList.remove("conversation__input--blur");
+  inputBox.addEventListener("focus", (event) => {
+    inputBox.classList.add("conversation__input--focus");
+    inputBox.classList.remove("conversation__input--blur");
   });
   //use blur event
-  input[i].addEventListener("blur", (event) => {
-    input[i].classList.add("conversation__input--blur");
-    input[i].classList.remove("conversation__input--focus");
+  inputBox.addEventListener("blur", (event) => {
+    inputBox.classList.add("conversation__input--blur");
+    inputBox.classList.remove("conversation__input--focus");
   });
-}
+});
